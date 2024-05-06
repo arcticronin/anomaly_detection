e3a713dd1b0d419f6f0c8b232fd75bd4b3d4af40
arcticronin <ronin.git@gmail.com>
2024-05-06 16:21:42 +0200

    DBSCAN and gower matrix

--- utils.py
+++ utils.py
@@ -1,19 +1,45 @@
-
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
-
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
+import torch.nn as nn
+import torch
+
+## globals
+alpha = 4/10
+binary_indices = [i for i in range(2, 16)]
+continuous_indices = [0] + [i for i in range(16, 20)]
+
+def dist(x, y): ## x :numpy array y: numpy array -> d : float
+    cd = np.mean(np.abs(x.iloc[continuous_indices]**2 - y.iloc[continuous_indices]**2))
+    bd = np.sum(x.iloc[binary_indices] != y.iloc[binary_indices])
+    d = alpha * cd + (1 - alpha) * bd  
+    return d
+
+
+class MixedLoss(nn.Module):
+    def __init__(self, alpha=alpha):
+        super(MixedLoss, self).__init__()
+        self.alpha = alpha
+    def forward(self, x, y):
+        cd = torch.mean(torch.abs(x[:, continuous_indices]**2 - y[:, continuous_indices]**2), dim=1)
+        bd = torch.sum(x[:, binary_indices] != y[:, binary_indices], dim=1)
+        d = (self.alpha * cd + (1 - self.alpha) * bd.float())
+        return d.mean()
+
 
 
 # Assume 'pca_df' contains your PCA results and you add 'labels' to this DataFrame
-def plot_TSNE(df, labels = None):
+def plot_TSNE(df, labels = None, dist_matrix = None):
     if labels is None:
         labels = np.ones(df.shape[0])
     # Applying t-SNE directly to your dataset
-    tsne = TSNE(n_components=2, verbose=1, perplexity=30, n_iter=1000)
+    if dist_matrix is None:
+        tsne = TSNE(n_components=2, verbose=1, perplexity=30, n_iter=1000)
+    else :
+        tsne = TSNE(n_components=2, verbose=1, perplexity=30, n_iter=1000, metric="precomputed", init='random')
     tsne_results = tsne.fit_transform(df)
     
     # Create a DataFrame to store results of t-SNE
@@ -21,10 +47,9 @@ def plot_TSNE(df, labels = None):
     tsne_df['labels'] = labels
     # Create a scatter plot with hue based on labels
     plt.figure(figsize=(8,6))
-    sns.scatterplot(data=tsne_df, x='TSNE1', y='TSNE2', hue='labels', palette='Set2', alpha=0.5)
+    sns.scatterplot(data=tsne_df, x='TSNE1', y='TSNE2', hue='labels', palette='Spectral', alpha=0.5)
     plt.title('t-SNE Visualization of Dataset with Labels')
     plt.show()
-
 
 
 # Assume 'pca_df' contains your PCA results and you add 'labels' to this DataFrame
@@ -43,3 +68,4 @@ def plot_PCA(df, labels = None):
     sns.scatterplot(data=pca_df, x='PC1', y='PC2', hue='labels', palette='Set2', alpha=0.5)
     plt.title('PCA of Dataset with Labels')
     plt.show()
+    
